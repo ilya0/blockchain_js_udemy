87fe7fddc706f6cb56c3e6a07a315a878ca66034
ilya0 <iosovets@gmail.com>
2020-01-17 09:38:47 -0800

    working block chain

--- dev/api.js
+++ dev/api.js
@@ -9,47 +9,51 @@ const bitcoin = new Blockchain(); // new blockchain instance
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-
 console.log("server running")
 
+
+
+//** home test */
 app.get('/', function(req,res){
     res.send('Hello World')
-    
 });
 
 
+// ** get the whole blockchain **
 app.get('/blockchain', function(req,res){
     res.send(bitcoin)
-    
 });
 
 
-
+// ** add a transactions **
 app.post('/transaction', function(req,res){
     const blockIndex = bitcoin.createNewTransaction(req.body.amount, req.body.sender)
     res.json({note:`Transaction will be added ${blockIndex}.`})
     console.log(req.body);
     //res.send('The amount of the transactions is ' ${req.body.amount} )
-    
 });
 
 
+//  ** mining a transaction **
 app.get('/mine', function(req,res){
+    //get the last block
     const lastBlock = bitcoin.getLastBlock();
-
+    //console.log("Last block is"+lastblock);
     const previousBlockHash = lastBlock['hash'];
-
-    const currentBlockData = {
+    //console.log("Previous block hash is"+previousBlockHash);
+        const currentBlockData = {
         transactions: bitcoin.pendingTransactions,
         index: lastBlock['index'] + 1
     };
+
     const nonce = bitcoin.proofOfWork(previousBlockHash,currentBlockData);
 
     const blockHash = bitcoin.hashBlock(previousBlockHash,currentBlockData,nonce);
 
-    bitcoin.createNewTransaction(12.5,"mineraddress", nodeAddress);
+    const newBlock = bitcoin.createNewBlock(nonce, previousBlockHash, blockHash);
 
-    const newBlock = bitcoin.createNewBlock(nonce, previousBlockHash, currentBlockData);
+  // reward miners for transaction
+    bitcoin.createNewTransaction(12.5,"mineraddress", nodeAddress);
 
     res.json({
         note: "New block mined successfully",
