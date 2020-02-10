cfe8f4afa224602dcf078e5fff160a33d5db0a15
ilya0 <iosovets@gmail.com>
2020-02-10 12:13:26 -0800

    added address search

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -295,6 +295,12 @@ app.get('/transaction/:transactionId', function(req,res){
 });
 
 app.get('/address/:address', function (req,res){
+    const address = req.params.address;
+    const addressData = bitcoin.getAddressData(address);
+
+    res.json({
+        addressData: addressData
+    })
 
 });
 
